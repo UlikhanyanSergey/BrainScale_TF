69635bc41d8bc444428d1f32c10d575e9f62bc9a
UlikhanyanSergey <sergeyulikhanyan@gmail.com>
2023-08-02 02:46:40 +0400

    Modified User_Data

--- user_data.sh
+++ user_data.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 sudo apt update
-sudo apt install nginx -y
+sudo apt install apt-transport-https -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install docker-ce -y
+sudo apt install awscli -y
+aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
+docker run -d -p 3000:3000 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
