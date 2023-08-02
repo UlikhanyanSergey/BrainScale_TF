5837e4298d6718a85acf7b38a3c69df6a4e8d3c6
UlikhanyanSergey <sergeyulikhanyan@gmail.com>
2023-08-02 20:27:55 +0400

    Added Dockerfile for app.js and modified user_data for building Docker image

--- user_data.sh
+++ user_data.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 sudo apt update
+
 sudo apt install apt-transport-https -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install docker-ce -y
+sudo gpasswd -a ubuntu docker
+newgrp docker
+sudo service docker enable
+
 sudo apt install awscli -y
-aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
-docker run -d -p 3000:3000 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
+
+git clone https://github.com/brainscalesolutions/brainscale-simple-app.git
+#aws ecr get-login-password --region eu-central-1 | docker login --username AWS --password-stdin 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
+#docker run -d -p 3000:3000 421279864461.dkr.ecr.eu-central-1.amazonaws.com/my-app:latest
+docker build -t my_app:latest .
+docker run -d -p 3000:3000 my_app:latest
