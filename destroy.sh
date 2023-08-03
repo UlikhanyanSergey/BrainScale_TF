571c1435a95a028bd4dd2c121b516dd847a2b3f8
UlikhanyanSergey <sergeyulikhanyan@gmail.com>
2023-08-04 02:06:02 +0400

    Modified destroy.sh file

--- destroy.sh
+++ destroy.sh
@@ -6,10 +6,17 @@ AWS_ACCOUNT_ID="421279864461"
 ECR_REPOSITORY="container-repo"
 IMAGE_TAG="latest"
 
+home="/home/"
+user=$(whoami)
+projectpath="/Downloads/BrainScale_TF/Terraform/"
+path="$home$user$projectpath"
+cd ${path}
+
 # Authenticate with ECR
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com
 
 # Delete the image from ECR
 aws ecr batch-delete-image --repository-name $ECR_REPOSITORY --image-ids imageTag=$IMAGE_TAG --region $AWS_REGION
 
+# Destroy the infrastructure
 terraform destroy
